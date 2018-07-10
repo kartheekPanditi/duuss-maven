42a1959845749ce5d5e0ae1b2c55ab07dea77466
kartheekPanditi <info.kartheek@gmail.com>
2018-07-10 20:36:36 +0530

    first commit

---
+++ src/main/java/com/programcreek/helloworld/services/HelloWorldService.java
@@ -0,0 +1,9 @@
+package com.programcreek.helloworld.services;
+import org.springframework.stereotype.Service;
+@Service("helloWorldService")
+public class HelloWorldService {
+
+	public static void main(String[] args) {
+		System.out.println("my name is kartheek");
+	}
+}
